19ac6548d3c51867470b3ff9f3fe8de739338ca8
Rusty Good <rwgood18@yahoo.com>
2015-03-03 22:41:30 -0600

    Start on center element with changing project pictures

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 document.getElementById('cat-clicker').onclick = function () {
 	document.getElementById('cat-list').style.display = "block";
 }
+
+
+$(document).ready(function(){
+    $("#p4").mouseover(function(){
+    	console.log($("#slides").src);
+        $("#slides").attr("src", 'img/web-opto.png');
+    });
+});
+
+$(document).ready(function(){
+    $("#p3").mouseover(function(){
+    	console.log($("#slides").src);
+        $("#slides").attr("src", 'img/arcade-game.png');
+    });
+});
